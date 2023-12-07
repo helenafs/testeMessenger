7139520c322be4e13d60c3e3cd45d9233214db09
Roihmatoun <roihmatoun5@gmail.com>
2023-12-07 16:09:05 +0100

    ajout barre détail + contenus multimédias

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -7,9 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ChatComponent {
    isMoreDetailsVisible=false;
+   isMultimediaSectionOpen = false; 
+   isConfidentialitySectionOpen = false; 
 
   toggleMoreDetails() {
     this.isMoreDetailsVisible = !this.isMoreDetailsVisible;
   }
 
+  toggleSection(section: string) {
+    if (section === 'multimedia') {
+        this.isMultimediaSectionOpen = !this.isMultimediaSectionOpen;
+        this.isConfidentialitySectionOpen = false;
+    } else if (section === 'confidentiality') {
+        this.isConfidentialitySectionOpen = !this.isConfidentialitySectionOpen;
+        this.isMultimediaSectionOpen = false;
+    
+}
+}
 }
